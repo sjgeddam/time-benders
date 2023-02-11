78901a1fb09b0712a1cb9f51ae45abff865b70c1
sjgeddam <sgeddam@utexas.edu>
2023-02-11 17:54:15 -0600

    Initial Commit

---
+++ TimeBenders/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TimeBenders
+//
+//  Created by Ram G on 2/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TimeBenders/TimeBendersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeBendersApp.swift
+//  TimeBenders
+//
+//  Created by Ram G on 2/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeBendersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
