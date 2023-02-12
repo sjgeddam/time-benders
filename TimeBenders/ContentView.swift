7c5f2a661af9b4175b7aa2db92de9956b7bd29f8
sjgeddam <sgeddam@utexas.edu>
2023-02-11 18:07:51 -0600

    Add properties & textfield for input value

--- TimeBenders/ContentView.swift
+++ TimeBenders/ContentView.swift
@@ -8,15 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputMetric = "seconds"
+    @State private var outputMetric = "minutes"
+    @State private var conversionVal = 0.0
+    @FocusState private var amountIsFocused : Bool
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Let's bend time!")
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount to convert", value: $conversionVal, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                } header: {
+                    Text("Enter amount to convert")
+                }
+            }
         }
-        .padding()
     }
 }
 
