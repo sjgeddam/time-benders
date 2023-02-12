cbb608b77a7ef0b285802356ddc6b286cb9557d9
sjgeddam <sgeddam@utexas.edu>
2023-02-11 18:09:37 -0600

    Add pickers for unit conversion options

--- TimeBenders/ContentView.swift
+++ TimeBenders/ContentView.swift
@@ -2,7 +2,7 @@
 //  ContentView.swift
 //  TimeBenders
 //
-//  Created by Ram G on 2/11/23.
+//  Created by Soujanya G on 2/11/23.
 //
 
 import SwiftUI
@@ -13,6 +13,7 @@ struct ContentView: View {
     @State private var outputMetric = "minutes"
     @State private var conversionVal = 0.0
     @FocusState private var amountIsFocused : Bool
+    let metricOptions = ["seconds", "minutes", "hours", "days"]
     
     var body: some View {
         NavigationView {
@@ -23,6 +24,28 @@ struct ContentView: View {
                         .focused($amountIsFocused)
                 } header: {
                     Text("Enter amount to convert")
+                }
+                
+                Section {
+                    Picker("From ", selection: $inputMetric) {
+                        ForEach(metricOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("Convert from ")
+                }
+                
+                Section {
+                    Picker("To ", selection: $outputMetric) {
+                        ForEach(metricOptions, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("To")
                 }
             }
         }
