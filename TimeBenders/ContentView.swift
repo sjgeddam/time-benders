7f5f9200c726fbb883ae25e1992693dbb0232727
sjgeddam <sgeddam@utexas.edu>
2023-02-11 18:10:43 -0600

    Compute & display conversion

--- TimeBenders/ContentView.swift
+++ TimeBenders/ContentView.swift
@@ -15,6 +15,28 @@ struct ContentView: View {
     @FocusState private var amountIsFocused : Bool
     let metricOptions = ["seconds", "minutes", "hours", "days"]
     
+    var computedVal : Double {
+        var initialVal = conversionVal;
+        if (inputMetric == "minutes") {
+            initialVal *= 60
+        } else if (inputMetric == "hours") {
+            initialVal *= 3600
+        } else if (inputMetric == "days") {
+            initialVal *= 86400
+        }
+        
+        
+        if (outputMetric == "minutes") {
+            return round(initialVal / 60 * 100) / 100.0
+        } else if (outputMetric == "hours") {
+            return round(initialVal / 3600 * 100) / 100.0
+        } else if (outputMetric == "days") {
+            return round(initialVal / 86400 * 100) / 100.0
+        }
+        
+        return initialVal
+    }
+    
     var body: some View {
         NavigationView {
             Form {
@@ -47,6 +69,11 @@ struct ContentView: View {
                 } header: {
                     Text("To")
                 }
+                
+                Section {
+                    Text("\(conversionVal.formatted()) \(inputMetric) is \(computedVal.formatted()) \(outputMetric)")
+                }
+
             }
         }
     }
